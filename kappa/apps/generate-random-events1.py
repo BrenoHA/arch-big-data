7999c820165a3f49a44a7707ef86aa52a28b7ea9
BrenoHA <breno.ha@gmail.com>
2024-03-15 16:04:45 +0100

    fix tabulation

--- kappa/apps/generate-random-events1.py
+++ kappa/apps/generate-random-events1.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python 
-
 import sys, time, json, random
 from datetime import datetime
 from kafka import KafkaProducer
@@ -8,21 +6,21 @@ def generate_random_event():
     now = datetime.now() # current date and time
     capteur = random.randrange(403, 406)
     if capteur == 405:
-    	x = {
+         x = {
 		"date": now.strftime("%Y-%m-%d %H:%M:%S"),
 		"numero": round(random.uniform(1, 4)),
         	"capteur": "humidite",
         	"valeur": round(random.uniform(50.00, 70.00), 2)
     	}
     if capteur == 403:
-    	x = {
+         x = {
 		"date": now.strftime("%Y-%m-%d %H:%M:%S"),
 		"numero": round(random.uniform(1, 4)),
         	"capteur": "pression",
         	"valeur": round(random.uniform(1015.00, 995.00))
     	}
     if capteur == 404:
-    	x = {
+         x = {
 		"date": now.strftime("%Y-%m-%d %H:%M:%S"),
 		"numero": round(random.uniform(1, 4)),
         	"capteur": "temperature",
@@ -31,7 +29,6 @@ def generate_random_event():
     return x
 
 def produce_to_kafka(data, topic):
-    value_serializer=lambda x: json.dumps(x).encode('utf-8')
     producer = KafkaProducer(
         bootstrap_servers=["localhost:9092"], 
         value_serializer=lambda x: json.dumps(x).encode('utf-8')
